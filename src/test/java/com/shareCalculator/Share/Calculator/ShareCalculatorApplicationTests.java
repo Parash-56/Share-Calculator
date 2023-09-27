9b15d6f977d71c99e56c7f43daaa79eeab8a63be
Parash-56 <parashdhakal023@gmail.com>
2023-09-27 23:24:35 +0545

    test

---
+++ src/test/java/com/shareCalculator/Share/Calculator/ShareCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shareCalculator.Share.Calculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShareCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
